2ece976ec634477cb733b300033f3391b4fded27
lahiru_lolitha <lahiru212001@gmail.com>
2023-11-05 01:12:06 +0530

    update 1.01

--- src/main/java/lk/ijse/AppIntializerWrapper.java
+++ src/main/java/lk/ijse/AppIntializerWrapper.java
@@ -1,4 +1,7 @@
 package lk.ijse;
 
 public class AppIntializerWrapper {
+    public static void main(String[] args) {
+        AppInitializer.main(args);
+    }
 }
---
+++ src/main/java/lk/ijse/controller/LoginFormController.java
@@ -0,0 +1,8 @@
+package lk.ijse.controller;
+
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+        private AnchorPane root;
+
+}
