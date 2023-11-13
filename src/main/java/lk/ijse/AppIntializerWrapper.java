1a55f37b5eea00c4eb34cac7d0603c9fc6c50d18
lahiru_lolitha <lahiru212001@gmail.com>
2023-11-14 03:36:57 +0530

    update 1.01

--- src/main/java/lk/ijse/AppIntializerWrapper.java
+++ src/main/java/lk/ijse/AppIntializerWrapper.java
@@ -1,6 +1,6 @@
 package lk.ijse;
 
-public class AppIntializerWrapper {
+public class AppIntializerWrapper {     //Start project class
     public static void main(String[] args) {
         AppInitializer.main(args);
     }
