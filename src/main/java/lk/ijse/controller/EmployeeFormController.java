97322f4d35f611f5171fda5165f194d0d796288a
lahiru_lolitha <lahiru212001@gmail.com>
2023-11-29 23:48:14 +0530

    employee

--- src/main/java/lk/ijse/controller/EmployeeFormController.java
+++ src/main/java/lk/ijse/controller/EmployeeFormController.java
@@ -189,6 +189,66 @@ public class EmployeeFormController {
     }
 
     public void btnEmployeeUpdateOnAction(ActionEvent actionEvent) {
+
+
+        if (!(txtEmployeerId.getText().isEmpty()||txtEmployeeName.getText().isEmpty()||txtEmployeeNIC.getText().isEmpty()||txtEmployeeAddress.getText().isEmpty())){
+            if (RegExPatterns.getEmployeeId().matcher(txtEmployeerId.getText()).matches()){
+                TxtColours.setDefaultColours(txtEmployeerId);
+                if (RegExPatterns.getNamePattern().matcher(txtEmployeeName.getText()).matches()){
+                    TxtColours.setDefaultColours(txtEmployeeName);
+                    if (RegExPatterns.getNICPattern().matcher(txtEmployeeNIC.getText()).matches()){
+                        TxtColours.setDefaultColours(txtEmployeeNIC);
+                        if (RegExPatterns.getAddressPattern().matcher(txtEmployeeAddress.getText()).matches()){
+                            TxtColours.setDefaultColours(txtEmployeeAddress);
+                        }else {
+                            TxtColours.setErrorColours(txtEmployeeAddress);
+                            new Alert(Alert.AlertType.ERROR,"Please Enter a valid Address!").showAndWait();
+                            return;
+                        }
+                    }else {
+                        TxtColours.setErrorColours(txtEmployeeNIC);
+                        new Alert(Alert.AlertType.ERROR,"Please Enter a valid NIC!").showAndWait();
+                        return;
+                    }
+                }else {
+                    TxtColours.setErrorColours(txtEmployeeName);
+                    new Alert(Alert.AlertType.ERROR,"Please Enter a valid Name!").showAndWait();
+                    return;
+                }
+            }else {
+                TxtColours.setErrorColours(txtEmployeerId);
+                new Alert(Alert.AlertType.ERROR,"Please Enter a valid Employee Id!").showAndWait();
+                return;
+            }
+        }else {
+            TxtColours.setErrorColours(txtEmployeeAddress);
+            TxtColours.setErrorColours(txtEmployeeNIC);
+            TxtColours.setErrorColours(txtEmployeeName);
+            TxtColours.setErrorColours(txtEmployeerId);
+            new SystemAlert(Alert.AlertType.WARNING,"Warrning","Please Enter the all Details").showAndWait();
+            return;
+        }
+        String employeeIDText = txtEmployeerId.getText();
+        String employeeNameText = txtEmployeeName.getText();
+        String employeeNICText = txtEmployeeNIC.getText();
+        String employeeAddressText = txtEmployeeAddress.getText();
+
+        EmployeeDto dto = new EmployeeDto(employeeIDText, employeeNameText, employeeNICText, employeeAddressText);
+        EmployeeModel model = new EmployeeModel();
+        try {
+            boolean isUpdated = model.updateEmployee(dto);
+            if (isUpdated){
+                loadAllEmployee();
+                new Alert(Alert.AlertType.CONFIRMATION,"Employee Updated!").showAndWait();
+                employeeFeildClear();
+            }else {
+                new Alert(Alert.AlertType.ERROR,"Employee Update Failed!").showAndWait();
+            }
+        }catch (SQLException e){
+            new Alert(Alert.AlertType.ERROR,e.getMessage()).showAndWait();
+        }
+
+
     }
     private void setEmployee(){
         tblEmployee.getSelectionModel().selectedItemProperty()
--- src/main/java/lk/ijse/model/EmployeeModel.java
+++ src/main/java/lk/ijse/model/EmployeeModel.java
@@ -18,12 +18,12 @@ public class EmployeeModel {
         String sql = "INSERT INTO employee values (?,?,?,?)";
         PreparedStatement pstm = connection.prepareStatement(sql);
 
-        pstm.setString(1,dto.getEmployeeid());
-        pstm.setString(2,dto.getEmployeeName());
-        pstm.setString(3,dto.getEmployeeNIC());
-        pstm.setString(4,dto.getEmployeeAddress());
+        pstm.setString(1, dto.getEmployeeid());
+        pstm.setString(2, dto.getEmployeeName());
+        pstm.setString(3, dto.getEmployeeNIC());
+        pstm.setString(4, dto.getEmployeeAddress());
 
-       boolean isSaved = pstm.executeUpdate()>0;
+        boolean isSaved = pstm.executeUpdate() > 0;
 
         return isSaved;
     }
@@ -31,21 +31,20 @@ public class EmployeeModel {
     public List<EmployeeDto> getAllEmployee() throws SQLException {
         Connection connection = DbConnection.getInstance().getConnection();
 
-        String sql= "SELECT * FROM employee ";
+        String sql = "SELECT * FROM employee ";
         PreparedStatement pstm = connection.prepareStatement(sql);
 
-        List<EmployeeDto> employeeDtos =new ArrayList<>();
+        List<EmployeeDto> employeeDtos = new ArrayList<>();
 
         ResultSet resultSet = pstm.executeQuery();
-        while (resultSet.next()){
-            String employee_id =resultSet.getString(1);
-            String employee_name =resultSet.getString(2);
-            String customer_nic =resultSet.getString(3);
-            String customer_address=resultSet.getString(4);
+        while (resultSet.next()) {
+            String employee_id = resultSet.getString(1);
+            String employee_name = resultSet.getString(2);
+            String customer_nic = resultSet.getString(3);
+            String customer_address = resultSet.getString(4);
 
 
-
-            EmployeeDto dto = new EmployeeDto(employee_id,employee_name,customer_nic,customer_address);
+            EmployeeDto dto = new EmployeeDto(employee_id, employee_name, customer_nic, customer_address);
             employeeDtos.add(dto);
         }
 
@@ -56,10 +55,10 @@ public class EmployeeModel {
         Connection connection = DbConnection.getInstance().getConnection();
         String sql = "SELECT * FROM employee WHERE employee_id = ?";
         PreparedStatement pstm = connection.prepareStatement(sql);
-        pstm.setString(1,employeeIDText);
+        pstm.setString(1, employeeIDText);
         ResultSet resultSet = pstm.executeQuery();
         EmployeeDto dto = null;
-        if (resultSet.next()){
+        if (resultSet.next()) {
             dto = new EmployeeDto(
                     resultSet.getString("employee_id"),
                     resultSet.getString("employee_name"),
@@ -71,5 +70,21 @@ public class EmployeeModel {
         }
 
         return dto;
+    }
+
+    public boolean updateEmployee(EmployeeDto dto) throws SQLException {
+
+        Connection connection = DbConnection.getInstance().getConnection();
+        String sql = "UPDATE employee SET employee_name=?,NIC=?,address=? WHERE employee_id=?";
+        PreparedStatement pstm = connection.prepareStatement(sql);
+
+        pstm.setString(1, dto.getEmployeeName());
+        pstm.setString(2, dto.getEmployeeNIC());
+        pstm.setString(3, dto.getEmployeeAddress());
+        pstm.setString(4, dto.getEmployeeid());
+
+        boolean isUpdated = pstm.executeUpdate() > 0;
+
+        return isUpdated;
     }
 }
